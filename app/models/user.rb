acf3a5760879829db6b89a4be6f9997846ca7c47
Faezeh Zand <faezeh.zand@sofatutor.com>
2025-10-13 14:17:34 +0330

    Add user model with password digest and migrations

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :mail, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a vaid email address" }, uniqueness: true
+end
