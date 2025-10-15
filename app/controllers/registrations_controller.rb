a8afcef72115208d7fd85fa3633cb3cc5c294e76
Faezeh Zand <faezeh.zand@sofatutor.com>
2025-10-15 21:45:02 +0330

    login and log out

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,9 +6,9 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if  @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "User created successfully"
     else
-      Rails.logger.info @user.errors.full_messages
       render :new
     end
   end
