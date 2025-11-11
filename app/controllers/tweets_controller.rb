d452131d5690fab881d023aa74d6d3c9e77024c2
Faezeh Zand <faezeh.zand@sofatutor.com>
2025-11-11 21:11:12 +0100

    Twitter API

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_tweet, only: [ :edit, :update, :destroy ]
 
   def index
     @tweets = Current.user.tweets
@@ -18,10 +19,29 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweet updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet deleted successfully."
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:body, :publish_at, :twitter_account_id)
   end
 
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
+  end
 end
